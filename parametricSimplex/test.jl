c94999912bec6be414694dd27555229b9aca677b
XavierG <xavier.gandibleux@univ-nantes.fr>
2017-06-19 08:36:16 +0200

    Update test.jl

--- parametricSimplex/test.jl
+++ parametricSimplex/test.jl
@@ -6,11 +6,19 @@ include("simplexparam_XG.jl")
 
 # Example 1 
 
+# min 3x1 +  x2
+# min -x1 - 2x2
+# s/c        x2 <= 3
+#     3x1 -  x2 <= 6
+#      x1 ,  x2 >= 0        
+
+# Data
+
 c1 = [ 3;  1; 0; 0]
 c2 = [-1; -2; 0; 0]
 T  = [ 0  1  1  0 ;
        3 -1  0  1 ]
-d = [3; 6]
+d  = [3; 6]
 
 c1 = Array{Float64}(c1)
 c2 = Array{Float64}(c2)
@@ -20,9 +28,11 @@ d  = Array{Float64}(d)
 println("Objective 1: ")
 @time opt_x1, obj1 = simplex_method(c1, T, d)
 println(" ")
+
 println("Objective 2: ")
 @time opt_x2, obj2 = simplex_method(c2, T, d)
 println(" ")
+
 println("Objective 1&2: ")
 @time  parametric_simplex_method(c1, c2, T, d)
 println(" ")
